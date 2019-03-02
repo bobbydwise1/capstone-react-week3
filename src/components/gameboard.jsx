264c21928998492b75f4b02dc7bcc4fcd3ce5c71
Robert Lee <bobbydwise1@hotmail.com>
2019-03-01 16:16:42 -0800

    Lift state of GameRoute Component.

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -13,7 +13,14 @@ function createSquareGrid(n) {
   return answer;
 }
 
-function Gameboard(){
+class Gameboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
   const compBox = {
     border: "solid blue",
     backgroundColor: "light yellow",
@@ -69,172 +76,171 @@ function Gameboard(){
   ]);
 
   //React method to render a grid via loop
+  render () {
+    return (
+      <div style={compBox}>
+        <h1>GAMEBOBARD</h1>
 
-  return (
-    <div style={compBox}>
-      <h1>GAMEBOBARD</h1>
+         <div style={gridContainer}>
+           <div style={gridItem}>{gameSystem._data[0][0]}</div>
+           <div style={gridItem}>{gameSystem._data[0][1]}</div>
+           <div style={gridItem}>{gameSystem._data[0][2]}</div>
+           <div style={gridItem}>{gameSystem._data[0][3]}</div>
+           <div style={gridItem}>{gameSystem._data[0][4]}</div>
+           <div style={gridItem}>{gameSystem._data[0][5]}</div>
+           <div style={gridItem}>{gameSystem._data[0][6]}</div>
+           <div style={gridItem}>{gameSystem._data[0][7]}</div>
+           <div style={gridItem}>{gameSystem._data[0][8]}</div>
+           <div style={gridItem}>{gameSystem._data[0][9]}</div>
+           <div style={gridItem}>{gameSystem._data[0][10]}</div>
+           <div style={gridItem}>{gameSystem._data[0][11]}</div>
+           <div style={gridItem}>{gameSystem._data[0][12]}</div>
 
-       <div style={gridContainer}>
-         <div style={gridItem}>{gameSystem._data[0][0]}</div>
-         <div style={gridItem}>{gameSystem._data[0][1]}</div>
-         <div style={gridItem}>{gameSystem._data[0][2]}</div>
-         <div style={gridItem}>{gameSystem._data[0][3]}</div>
-         <div style={gridItem}>{gameSystem._data[0][4]}</div>
-         <div style={gridItem}>{gameSystem._data[0][5]}</div>
-         <div style={gridItem}>{gameSystem._data[0][6]}</div>
-         <div style={gridItem}>{gameSystem._data[0][7]}</div>
-         <div style={gridItem}>{gameSystem._data[0][8]}</div>
-         <div style={gridItem}>{gameSystem._data[0][9]}</div>
-         <div style={gridItem}>{gameSystem._data[0][10]}</div>
-         <div style={gridItem}>{gameSystem._data[0][11]}</div>
-         <div style={gridItem}>{gameSystem._data[0][12]}</div>
+           <div style={gridItem}>{gameSystem._data[1][0]}</div>
+           <div style={gridItem}>{gameSystem._data[1][1]}</div>
+           <div style={gridItem}>{gameSystem._data[1][2]}</div>
+           <div style={gridItem}>{gameSystem._data[1][3]}</div>
+           <div style={gridItem}>{gameSystem._data[1][4]}</div>
+           <div style={gridItem}>{gameSystem._data[1][5]}</div>
+           <div style={gridItem}>{gameSystem._data[1][6]}</div>
+           <div style={gridItem}>{gameSystem._data[1][7]}</div>
+           <div style={gridItem}>{gameSystem._data[1][8]}</div>
+           <div style={gridItem}>{gameSystem._data[1][9]}</div>
+           <div style={gridItem}>{gameSystem._data[1][10]}</div>
+           <div style={gridItem}>{gameSystem._data[1][11]}</div>
+           <div style={gridItem}>{gameSystem._data[1][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[1][0]}</div>
-         <div style={gridItem}>{gameSystem._data[1][1]}</div>
-         <div style={gridItem}>{gameSystem._data[1][2]}</div>
-         <div style={gridItem}>{gameSystem._data[1][3]}</div>
-         <div style={gridItem}>{gameSystem._data[1][4]}</div>
-         <div style={gridItem}>{gameSystem._data[1][5]}</div>
-         <div style={gridItem}>{gameSystem._data[1][6]}</div>
-         <div style={gridItem}>{gameSystem._data[1][7]}</div>
-         <div style={gridItem}>{gameSystem._data[1][8]}</div>
-         <div style={gridItem}>{gameSystem._data[1][9]}</div>
-         <div style={gridItem}>{gameSystem._data[1][10]}</div>
-         <div style={gridItem}>{gameSystem._data[1][11]}</div>
-         <div style={gridItem}>{gameSystem._data[1][12]}</div>
+           <div style={gridItem}>{gameSystem._data[2][0]}</div>
+           <div style={gridItem}>{gameSystem._data[2][1]}</div>
+           <div style={gridItem}>{gameSystem._data[2][2]}</div>
+           <div style={gridItem}>{gameSystem._data[2][3]}</div>
+           <div style={gridItem}>{gameSystem._data[2][4]}</div>
+           <div style={gridItem}>{gameSystem._data[2][5]}</div>
+           <div style={gridItem}>{gameSystem._data[2][6]}</div>
+           <div style={gridItem}>{gameSystem._data[2][7]}</div>
+           <div style={gridItem}>{gameSystem._data[2][8]}</div>
+           <div style={gridItem}>{gameSystem._data[2][9]}</div>
+           <div style={gridItem}>{gameSystem._data[2][10]}</div>
+           <div style={gridItem}>{gameSystem._data[2][11]}</div>
+           <div style={gridItem}>{gameSystem._data[2][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[2][0]}</div>
-         <div style={gridItem}>{gameSystem._data[2][1]}</div>
-         <div style={gridItem}>{gameSystem._data[2][2]}</div>
-         <div style={gridItem}>{gameSystem._data[2][3]}</div>
-         <div style={gridItem}>{gameSystem._data[2][4]}</div>
-         <div style={gridItem}>{gameSystem._data[2][5]}</div>
-         <div style={gridItem}>{gameSystem._data[2][6]}</div>
-         <div style={gridItem}>{gameSystem._data[2][7]}</div>
-         <div style={gridItem}>{gameSystem._data[2][8]}</div>
-         <div style={gridItem}>{gameSystem._data[2][9]}</div>
-         <div style={gridItem}>{gameSystem._data[2][10]}</div>
-         <div style={gridItem}>{gameSystem._data[2][11]}</div>
-         <div style={gridItem}>{gameSystem._data[2][12]}</div>
+           <div style={gridItem}>{gameSystem._data[3][0]}</div>
+           <div style={gridItem}>{gameSystem._data[3][1]}</div>
+           <div style={gridItem}>{gameSystem._data[3][2]}</div>
+           <div style={gridItem}>{gameSystem._data[3][3]}</div>
+           <div style={gridItem}>{gameSystem._data[3][4]}</div>
+           <div style={gridItem}>{gameSystem._data[3][5]}</div>
+           <div style={gridItem}>{gameSystem._data[3][6]}</div>
+           <div style={gridItem}>{gameSystem._data[3][7]}</div>
+           <div style={gridItem}>{gameSystem._data[3][8]}</div>
+           <div style={gridItem}>{gameSystem._data[3][9]}</div>
+           <div style={gridItem}>{gameSystem._data[3][10]}</div>
+           <div style={gridItem}>{gameSystem._data[3][11]}</div>
+           <div style={gridItem}>{gameSystem._data[3][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[3][0]}</div>
-         <div style={gridItem}>{gameSystem._data[3][1]}</div>
-         <div style={gridItem}>{gameSystem._data[3][2]}</div>
-         <div style={gridItem}>{gameSystem._data[3][3]}</div>
-         <div style={gridItem}>{gameSystem._data[3][4]}</div>
-         <div style={gridItem}>{gameSystem._data[3][5]}</div>
-         <div style={gridItem}>{gameSystem._data[3][6]}</div>
-         <div style={gridItem}>{gameSystem._data[3][7]}</div>
-         <div style={gridItem}>{gameSystem._data[3][8]}</div>
-         <div style={gridItem}>{gameSystem._data[3][9]}</div>
-         <div style={gridItem}>{gameSystem._data[3][10]}</div>
-         <div style={gridItem}>{gameSystem._data[3][11]}</div>
-         <div style={gridItem}>{gameSystem._data[3][12]}</div>
+           <div style={gridItem}>{gameSystem._data[4][0]}</div>
+           <div style={gridItem}>{gameSystem._data[4][1]}</div>
+           <div style={gridItem}>{gameSystem._data[4][2]}</div>
+           <div style={gridItem}>{gameSystem._data[4][3]}</div>
+           <div style={gridItem}>{gameSystem._data[4][4]}</div>
+           <div style={gridItem}>{gameSystem._data[4][5]}</div>
+           <div style={gridItem}>{gameSystem._data[4][6]}</div>
+           <div style={gridItem}>{gameSystem._data[4][7]}</div>
+           <div style={gridItem}>{gameSystem._data[4][8]}</div>
+           <div style={gridItem}>{gameSystem._data[4][9]}</div>
+           <div style={gridItem}>{gameSystem._data[4][10]}</div>
+           <div style={gridItem}>{gameSystem._data[4][11]}</div>
+           <div style={gridItem}>{gameSystem._data[4][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[4][0]}</div>
-         <div style={gridItem}>{gameSystem._data[4][1]}</div>
-         <div style={gridItem}>{gameSystem._data[4][2]}</div>
-         <div style={gridItem}>{gameSystem._data[4][3]}</div>
-         <div style={gridItem}>{gameSystem._data[4][4]}</div>
-         <div style={gridItem}>{gameSystem._data[4][5]}</div>
-         <div style={gridItem}>{gameSystem._data[4][6]}</div>
-         <div style={gridItem}>{gameSystem._data[4][7]}</div>
-         <div style={gridItem}>{gameSystem._data[4][8]}</div>
-         <div style={gridItem}>{gameSystem._data[4][9]}</div>
-         <div style={gridItem}>{gameSystem._data[4][10]}</div>
-         <div style={gridItem}>{gameSystem._data[4][11]}</div>
-         <div style={gridItem}>{gameSystem._data[4][12]}</div>
+           <div style={gridItem}>{gameSystem._data[5][0]}</div>
+           <div style={gridItem}>{gameSystem._data[5][1]}</div>
+           <div style={gridItem}>{gameSystem._data[5][2]}</div>
+           <div style={gridItem}>{gameSystem._data[5][3]}</div>
+           <div style={gridItem}>{gameSystem._data[5][4]}</div>
+           <div style={gridItem}>{gameSystem._data[5][5]}</div>
+           <div style={gridItem}>{gameSystem._data[5][6]}</div>
+           <div style={gridItem}>{gameSystem._data[5][7]}</div>
+           <div style={gridItem}>{gameSystem._data[5][8]}</div>
+           <div style={gridItem}>{gameSystem._data[5][9]}</div>
+           <div style={gridItem}>{gameSystem._data[5][10]}</div>
+           <div style={gridItem}>{gameSystem._data[5][11]}</div>
+           <div style={gridItem}>{gameSystem._data[5][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[5][0]}</div>
-         <div style={gridItem}>{gameSystem._data[5][1]}</div>
-         <div style={gridItem}>{gameSystem._data[5][2]}</div>
-         <div style={gridItem}>{gameSystem._data[5][3]}</div>
-         <div style={gridItem}>{gameSystem._data[5][4]}</div>
-         <div style={gridItem}>{gameSystem._data[5][5]}</div>
-         <div style={gridItem}>{gameSystem._data[5][6]}</div>
-         <div style={gridItem}>{gameSystem._data[5][7]}</div>
-         <div style={gridItem}>{gameSystem._data[5][8]}</div>
-         <div style={gridItem}>{gameSystem._data[5][9]}</div>
-         <div style={gridItem}>{gameSystem._data[5][10]}</div>
-         <div style={gridItem}>{gameSystem._data[5][11]}</div>
-         <div style={gridItem}>{gameSystem._data[5][12]}</div>
+           <div style={gridItem}>{gameSystem._data[6][0]}</div>
+           <div style={gridItem}>{gameSystem._data[6][1]}</div>
+           <div style={gridItem}>{gameSystem._data[6][2]}</div>
+           <div style={gridItem}>{gameSystem._data[6][3]}</div>
+           <div style={gridItem}>{gameSystem._data[6][4]}</div>
+           <div style={gridItem}>{gameSystem._data[6][5]}</div>
+           <div style={gridItem}>{gameSystem._data[6][6]}</div>
+           <div style={gridItem}>{gameSystem._data[6][7]}</div>
+           <div style={gridItem}>{gameSystem._data[6][8]}</div>
+           <div style={gridItem}>{gameSystem._data[6][9]}</div>
+           <div style={gridItem}>{gameSystem._data[6][10]}</div>
+           <div style={gridItem}>{gameSystem._data[6][11]}</div>
+           <div style={gridItem}>{gameSystem._data[6][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[6][0]}</div>
-         <div style={gridItem}>{gameSystem._data[6][1]}</div>
-         <div style={gridItem}>{gameSystem._data[6][2]}</div>
-         <div style={gridItem}>{gameSystem._data[6][3]}</div>
-         <div style={gridItem}>{gameSystem._data[6][4]}</div>
-         <div style={gridItem}>{gameSystem._data[6][5]}</div>
-         <div style={gridItem}>{gameSystem._data[6][6]}</div>
-         <div style={gridItem}>{gameSystem._data[6][7]}</div>
-         <div style={gridItem}>{gameSystem._data[6][8]}</div>
-         <div style={gridItem}>{gameSystem._data[6][9]}</div>
-         <div style={gridItem}>{gameSystem._data[6][10]}</div>
-         <div style={gridItem}>{gameSystem._data[6][11]}</div>
-         <div style={gridItem}>{gameSystem._data[6][12]}</div>
+           <div style={gridItem}>{gameSystem._data[7][0]}</div>
+           <div style={gridItem}>{gameSystem._data[7][1]}</div>
+           <div style={gridItem}>{gameSystem._data[7][2]}</div>
+           <div style={gridItem}>{gameSystem._data[7][3]}</div>
+           <div style={gridItem}>{gameSystem._data[7][4]}</div>
+           <div style={gridItem}>{gameSystem._data[7][5]}</div>
+           <div style={gridItem}>{gameSystem._data[7][6]}</div>
+           <div style={gridItem}>{gameSystem._data[7][7]}</div>
+           <div style={gridItem}>{gameSystem._data[7][8]}</div>
+           <div style={gridItem}>{gameSystem._data[7][9]}</div>
+           <div style={gridItem}>{gameSystem._data[7][10]}</div>
+           <div style={gridItem}>{gameSystem._data[7][11]}</div>
+           <div style={gridItem}>{gameSystem._data[7][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[7][0]}</div>
-         <div style={gridItem}>{gameSystem._data[7][1]}</div>
-         <div style={gridItem}>{gameSystem._data[7][2]}</div>
-         <div style={gridItem}>{gameSystem._data[7][3]}</div>
-         <div style={gridItem}>{gameSystem._data[7][4]}</div>
-         <div style={gridItem}>{gameSystem._data[7][5]}</div>
-         <div style={gridItem}>{gameSystem._data[7][6]}</div>
-         <div style={gridItem}>{gameSystem._data[7][7]}</div>
-         <div style={gridItem}>{gameSystem._data[7][8]}</div>
-         <div style={gridItem}>{gameSystem._data[7][9]}</div>
-         <div style={gridItem}>{gameSystem._data[7][10]}</div>
-         <div style={gridItem}>{gameSystem._data[7][11]}</div>
-         <div style={gridItem}>{gameSystem._data[7][12]}</div>
+           <div style={gridItem}>{gameSystem._data[8][0]}</div>
+           <div style={gridItem}>{gameSystem._data[8][1]}</div>
+           <div style={gridItem}>{gameSystem._data[8][2]}</div>
+           <div style={gridItem}>{gameSystem._data[8][3]}</div>
+           <div style={gridItem}>{gameSystem._data[8][4]}</div>
+           <div style={gridItem}>{gameSystem._data[8][5]}</div>
+           <div style={gridItem}>{gameSystem._data[8][6]}</div>
+           <div style={gridItem}>{gameSystem._data[8][7]}</div>
+           <div style={gridItem}>{gameSystem._data[8][8]}</div>
+           <div style={gridItem}>{gameSystem._data[8][9]}</div>
+           <div style={gridItem}>{gameSystem._data[8][10]}</div>
+           <div style={gridItem}>{gameSystem._data[8][11]}</div>
+           <div style={gridItem}>{gameSystem._data[8][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[8][0]}</div>
-         <div style={gridItem}>{gameSystem._data[8][1]}</div>
-         <div style={gridItem}>{gameSystem._data[8][2]}</div>
-         <div style={gridItem}>{gameSystem._data[8][3]}</div>
-         <div style={gridItem}>{gameSystem._data[8][4]}</div>
-         <div style={gridItem}>{gameSystem._data[8][5]}</div>
-         <div style={gridItem}>{gameSystem._data[8][6]}</div>
-         <div style={gridItem}>{gameSystem._data[8][7]}</div>
-         <div style={gridItem}>{gameSystem._data[8][8]}</div>
-         <div style={gridItem}>{gameSystem._data[8][9]}</div>
-         <div style={gridItem}>{gameSystem._data[8][10]}</div>
-         <div style={gridItem}>{gameSystem._data[8][11]}</div>
-         <div style={gridItem}>{gameSystem._data[8][12]}</div>
+           <div style={gridItem}>{gameSystem._data[9][0]}</div>
+           <div style={gridItem}>{gameSystem._data[9][1]}</div>
+           <div style={gridItem}>{gameSystem._data[9][2]}</div>
+           <div style={gridItem}>{gameSystem._data[9][3]}</div>
+           <div style={gridItem}>{gameSystem._data[9][4]}</div>
+           <div style={gridItem}>{gameSystem._data[9][5]}</div>
+           <div style={gridItem}>{gameSystem._data[9][6]}</div>
+           <div style={gridItem}>{gameSystem._data[9][7]}</div>
+           <div style={gridItem}>{gameSystem._data[9][8]}</div>
+           <div style={gridItem}>{gameSystem._data[9][9]}</div>
+           <div style={gridItem}>{gameSystem._data[9][10]}</div>
+           <div style={gridItem}>{gameSystem._data[9][11]}</div>
+           <div style={gridItem}>{gameSystem._data[9][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[9][0]}</div>
-         <div style={gridItem}>{gameSystem._data[9][1]}</div>
-         <div style={gridItem}>{gameSystem._data[9][2]}</div>
-         <div style={gridItem}>{gameSystem._data[9][3]}</div>
-         <div style={gridItem}>{gameSystem._data[9][4]}</div>
-         <div style={gridItem}>{gameSystem._data[9][5]}</div>
-         <div style={gridItem}>{gameSystem._data[9][6]}</div>
-         <div style={gridItem}>{gameSystem._data[9][7]}</div>
-         <div style={gridItem}>{gameSystem._data[9][8]}</div>
-         <div style={gridItem}>{gameSystem._data[9][9]}</div>
-         <div style={gridItem}>{gameSystem._data[9][10]}</div>
-         <div style={gridItem}>{gameSystem._data[9][11]}</div>
-         <div style={gridItem}>{gameSystem._data[9][12]}</div>
+           <div style={gridItem}>{gameSystem._data[10][0]}</div>
+           <div style={gridItem}>{gameSystem._data[10][1]}</div>
+           <div style={gridItem}>{gameSystem._data[10][2]}</div>
+           <div style={gridItem}>{gameSystem._data[10][3]}</div>
+           <div style={gridItem}>{gameSystem._data[10][4]}</div>
+           <div style={gridItem}>{gameSystem._data[10][5]}</div>
+           <div style={gridItem}>{gameSystem._data[10][6]}</div>
+           <div style={gridItem}>{gameSystem._data[10][7]}</div>
+           <div style={gridItem}>{gameSystem._data[10][8]}</div>
+           <div style={gridItem}>{gameSystem._data[10][9]}</div>
+           <div style={gridItem}>{gameSystem._data[10][10]}</div>
+           <div style={gridItem}>{gameSystem._data[10][11]}</div>
+           <div style={gridItem}>{gameSystem._data[10][12]}</div>
 
-         <div style={gridItem}>{gameSystem._data[10][0]}</div>
-         <div style={gridItem}>{gameSystem._data[10][1]}</div>
-         <div style={gridItem}>{gameSystem._data[10][2]}</div>
-         <div style={gridItem}>{gameSystem._data[10][3]}</div>
-         <div style={gridItem}>{gameSystem._data[10][4]}</div>
-         <div style={gridItem}>{gameSystem._data[10][5]}</div>
-         <div style={gridItem}>{gameSystem._data[10][6]}</div>
-         <div style={gridItem}>{gameSystem._data[10][7]}</div>
-         <div style={gridItem}>{gameSystem._data[10][8]}</div>
-         <div style={gridItem}>{gameSystem._data[10][9]}</div>
-         <div style={gridItem}>{gameSystem._data[10][10]}</div>
-         <div style={gridItem}>{gameSystem._data[10][11]}</div>
-         <div style={gridItem}>{gameSystem._data[10][12]}</div>
+         </div>
 
-       </div>
+      </div>
 
-    </div>
-  );
-}
-
-
+    );
+  }
 
 export default Gameboard;
